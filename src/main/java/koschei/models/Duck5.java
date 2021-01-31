505379424b9290461830a550392c459714cfb802
VeraPolyanskikh <vpcat3@gmail.com>
2021-01-31 18:24:48 +0300

    Spring Task 3

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,18 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class Duck5 {
+
+    private Egg6 egg;
+
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
